3d037b937cce534135d6bc94f06173a93513e925
Youssef Elezaby <youssefel3zaby@gmail.com>
2025-03-31 00:23:13 +0200

    start working on admin page

---
+++ app/admin/layout.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
+import "@/styles/admin.css";
+import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
+
+const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  if (!session?.user?.id) redirect("/sign-in");
+  return (
+    <main className="flex min-h-screen w-full flex-row">
+      <Sidebar session={session} />
+      <div className="admin-container">
+        <Header session={session} />
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default layout;
---
+++ components/admin/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Session } from "next-auth";
+const Header = ({ session }: { session: Session }) => {
+  return (
+    <header className="admin-header">
+      <div>
+        <h2 className="text-dark-400 font-semibold text-2xl">
+          {session?.user?.name}
+        </h2>
+        <p className="text-base text-slate-500">
+          Monitor all of your users and books
+        </p>
+      </div>
+      <p>Search</p>
+    </header>
+  );
+};
+
+export default Header;
